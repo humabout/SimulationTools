a29b69be9b9029c9a336638b9f696f4e3d9326b7
Timothy <humabout@gmail.com>
2020-11-19 16:57:31 -0700

    Started work on building up a multivector library

---
+++ Source/Utilities/Math/vector_3d.h
@@ -0,0 +1,26 @@
+// vector_3d.h
+
+
+#ifndef VECTOR_3D_H
+#define VECTOR_3D_H
+
+
+// Inclusions
+
+
+//------------------------------------------------------------------------------
+// Name:    math
+// Purpose: This namespace holds all parts of the simulation kernel.
+//------------------------------------------------------------------------------
+namespace math
+{
+
+
+
+
+
+
+} // !math
+
+
+#endif // !VECTOR_3D_H
