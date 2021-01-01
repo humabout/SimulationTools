5b2dcd8267522f6dd9043795de064f893e4e85bd
Timothy <humabout@gmail.com>
2020-12-31 19:34:09 -0700

    Added a single header that contains all the inclusions needed for the math library to operate. This provides a single point of access to the libarry so the user doesn't have to include dozens of files to get the functionality they want.

---
+++ Source/math.h
@@ -0,0 +1,19 @@
+// math.h
+
+
+// This file provides all inclusions needed to use the math libraries included
+// in the Nemesis Toolkit. Other parts of the math library are portioned out in
+// separate libraries.
+#ifndef NEMESIS_MATH_H
+#define NEMESIS_MATH_H
+
+
+// Inclusions
+#include "Math/math_config.h"
+#include "Math/math_constants.h"
+#include "Math/typedefs.h"
+#include "Math/mat.h"
+#include "Math/vec.h"
+
+
+#endif // !NEMESIS_MATH_H
